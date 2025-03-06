a11184e0e4212778062987188eb1c683510d5d03
Vishu8919 <vishnuthulasi96@gmail.com>
2025-03-07 00:18:44 +0530

    first Commit

---
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -0,0 +1,15 @@
+import Header from "../Common/Header";
+
+const UserLayout = () => {
+    return <>
+    {/* Header */}
+    <Header/>
+
+    {/* Main content */}
+
+    {/* Footer */}
+
+    
+    </>
+};
+export default UserLayout;
